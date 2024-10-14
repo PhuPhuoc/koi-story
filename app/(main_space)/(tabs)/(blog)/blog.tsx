20ee1d840bd8c2985c55646870aed75ec54e920d
nhantam2502 <nhantamhd@gmail.com>
2024-10-14 22:18:29 +0700

    Done UI Carousel

--- app/(main_space)/(tabs)/(blog)/blog.tsx
+++ app/(main_space)/(tabs)/(blog)/blog.tsx
@@ -1,6 +1,7 @@
 
 import { Link } from "expo-router"
 import { Text, View } from "react-native"
+import CarouselComponent from "../../../../components/carousel/carousel"
 
 const BlogPage = () => {
     const id = "blog title 1"
@@ -8,6 +9,7 @@ const BlogPage = () => {
         <View>
             <Text>BlogPage</Text>
             <Link href={`/(main_space)/blog_detail/${id}`}>See Detail blog_detail_demo</Link>
+            <CarouselComponent/>
         </View>
     )
 }
--- components/carousel/carousel.tsx
+++ components/carousel/carousel.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
 
-const Carousel = () => {
-    return (
-        <View>
-            <Text>Carousel</Text>
-        </View>
-    )
-}
+const CarouselComponent = () => {
+  const width = Dimensions.get("window").width;
 
-export default Carousel
+  const list = [
+    {
+      id: 1,
+      title: "Koi",
+      image: require("../../assets/koi.jpg"),
+    },
+    {
+      id: 2,
+      title: "Koi 2",
+      image: require("../../assets/koi2.jpg"),
+    },
+    {
+      id: 3,
+      title: "Koi 3",
+      image: require("../../assets/koi3.jpg"),
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <Carousel
+        width={width}
+        height={width / 2}
+        data={list}
+        autoPlay={true}
+        autoPlayInterval={3000}
+        mode="parallax"
+        modeConfig={{
+          parallaxScrollingScale: 0.9,
+          parallaxScrollingOffset: 50,
+        }}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.carouselItem}>
+              <Image style={styles.img} source={item.image} />
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 20,
+  },
+  carouselItem: {
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
+  },
+});
+
+export default CarouselComponent;
