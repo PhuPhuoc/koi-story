92b863a83a68f3b935d48ecb8c1f57d1c4e2b976
DatPham1103 <phamtdat1103@gmail.com>
2024-10-24 21:28:30 +0700

    first commit of branch Dat

--- app/(main_space)/(tabs)/(blog)/blog.tsx
+++ app/(main_space)/(tabs)/(blog)/blog.tsx
@@ -1,15 +1,84 @@
-
-import { Link } from "expo-router"
-import { Text, View } from "react-native"
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import dummy from "../../../../dummy_data/dummy_blog.json";
 
 const BlogPage = () => {
-    const id = "blog title 1"
-    return (
-        <View>
-            <Text>BlogPage</Text>
-            <Link href={`/(main_space)/blog_detail/${id}`}>See Detail blog_detail_demo</Link>
-        </View>
-    )
-}
+  const renderItem = ({
+    item,
+  }: {
+    item: {
+      id: number;
+      title: string;
+      author: string;
+      content: string;
+      image: string;
+    };
+  }) => (
+    <TouchableOpacity style={styles.card} onPress={() => console.log()}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.artName}>{item.title}</Text>
+        <Text>{item.author}</Text>
+        <Text style={styles.description} numberOfLines={2}>
+          {item.content}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 
-export default BlogPage
+  return (
+    <FlatList
+      data={dummy}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={styles.container}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    paddingBottom: 100,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  image: {
+    width: "100%",
+    height: 150,
+    borderRadius: 8,
+  },
+  textContainer: {
+    marginTop: 10,
+  },
+  artName: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  price: {
+    fontSize: 16,
+    color: "#888",
+    marginVertical: 5,
+  },
+  description: {
+    fontSize: 14,
+    color: "#666",
+  },
+});
+
+export default BlogPage;
