1c7dd1ba6a3328eb578af650a04cc026d4a022f7
RedQW <danhtang226@gmail.com>
2024-10-27 14:08:26 +0700

    add myconsult form

--- app/(main_space)/(tabs)/(member)/my_consult.tsx
+++ app/(main_space)/(tabs)/(member)/my_consult.tsx
@@ -1,12 +1,78 @@
-
-import { Text, View } from "react-native"
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View, StyleSheet, Modal } from "react-native";
+import AddConsult from "../../../../components/my_consult/AddConsultModal";
 
 const MyConsultPage = () => {
-    return (
-        <View>
-            <Text>MyConsultPage</Text>
-        </View>
-    )
-}
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-export default MyConsultPage
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.floatingButton} onPress={openModal}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <AddConsult closeModal={closeModal} />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  floatingButton: {
+    position: "absolute",
+    right: 20,
+    top: 20,
+    backgroundColor: "#9BB0C1",
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: 370,
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+  },
+});
+
+export default MyConsultPage;
