21c49910f2c9cc81303ef6107f820d2362aec86d
nhantam2502 <nhantamhd@gmail.com>
2024-10-31 09:59:08 +0700

    Import post_id to Feedback

--- app/(main_space)/post_market_detail/[id].tsx
+++ app/(main_space)/post_market_detail/[id].tsx
@@ -198,7 +198,7 @@ const PostMarketDetail = () => {
         {marketData && renderKoiInfo(marketData.color, marketData.origin)}
 
         <GestureHandlerRootView style={styles.container}>
-          <Feedback/>
+          <Feedback post_id={dummyId}/>
         </GestureHandlerRootView>
       </ScrollView>
     </View>
